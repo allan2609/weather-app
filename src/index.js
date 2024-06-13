d335aa10fa2fb3a99d889d2cdb8966fb7d0ac277
Allan Volmer <115582490+allan2609@users.noreply.github.com>
2024-06-13 15:23:52 +0300

    Write function to get temperature

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 import "./styles.css";
 
-console.log("it works")
+async function getTemperature(searchTerm = "Pärnu") {
+
+  try {
+    const response = await fetch(`https://api.weatherapi.com/v1/current.json?key=8b91481cdf6a423f8c7113257242805&q=${searchTerm}`, { mode: "cors" });
+    
+    if (!response.ok) {
+      throw new Error("Network response was not ok");
+    }
+    
+    const data = await response.json();
+    
+    console.log(`Current temperature in ${searchTerm} is ` + data.current.temp_c + "C");
+  } catch (error) {
+    console.error("Error fetching data:", error);
+  }
+}
+
+getTemperature();
